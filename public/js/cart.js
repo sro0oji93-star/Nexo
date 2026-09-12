669e201701b65f88383b7a1b61d3600101be853c
sro0oji93-star <sro0oji93@gmail.com>
2026-09-12 19:50:03 +0200

    Warenkorb-Anmerkung groesser (Textarea) + Pizza-Groessen mobil in einer Reihe

--- public/js/cart.js
+++ public/js/cart.js
@@ -853,7 +853,7 @@ var Cart = (function() {
         }).join('') + '</div>';
       }
       var noteVal = item.note ? escapeHtml(item.note) : '';
-      var noteHtml = '<input type="text" class="co-note" data-key="' + escapeHtml(item._key) + '" value="' + noteVal + '" maxlength="200" placeholder="Anmerkung zu diesem Artikel…" style="margin-top:6px;width:100%;max-width:280px;padding:6px 10px;border:1px solid #e8e4de;border-radius:8px;font-size:12px;color:#232223;background:#fff">';
+      var noteHtml = '<textarea class="co-note" data-key="' + escapeHtml(item._key) + '" maxlength="200" rows="2" placeholder="Anmerkung, z.B. ohne Zwiebeln, extra scharf…">' + noteVal + '</textarea>';
       return '<div class="cart-item">' +
         '<div class="cart-item-image"><i class="fas fa-utensils"></i></div>' +
         '<div class="cart-item-info"><h4>' + nameHtml + '</h4>' + extrasHtml + noteHtml + '</div>' +
